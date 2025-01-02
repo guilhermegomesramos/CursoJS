a82525b394ef81e6ba5e11d0eedd037192613408
guilhermegomesramos <168496717+guilhermegomesramos@users.noreply.github.com>
2025-01-02 11:25:19 -0300

    .

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log(`a velocidade é ${vel}km/h`)
+if(vel > 60) {
+    console.log('você utrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais} você é: `)
+if(pais == 'Brasil') {
+    console.log('brasileiro')
+} else {
+    console.log('estrangeiro')
+}
